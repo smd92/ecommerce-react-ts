01f414decad1b00a3e33f234ebacb9de5b22ff14
smd92 <69214590+smd92@users.noreply.github.com>
2024-10-05 15:03:57 +0200

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <h1 className="text-3xl font-bold underline text-cyan-600">Hello world!</h1>
+  );
 };
 
 export default App;
