32714acd2798b5fb39af1a150d990423323d1fbc
smd92 <69214590+smd92@users.noreply.github.com>
2024-10-06 16:05:13 +0200

    .

--- client/src/scenes/ProductsGrid.tsx
+++ client/src/scenes/ProductsGrid.tsx
@@ -5,7 +5,7 @@ const ProductsGrid = () => {
   const rowCount = Math.ceil(mockProductData.length / 4);
   return (
     <div
-      className={`w-10/12 grid grid-cols-4 grid-rows-${rowCount} items-center gap-6 bg-color-theme1 my-8`}
+      className={`w-10/12 grid grid-cols-4 grid-rows-${rowCount} items-center gap-6 bg-color-alternative-theme1 my-8`}
     >
       {mockProductData.map((product) => (
         <ProductCard key={product.id} {...product} />
